6ed669a873716eeff2a061a32692d2e7ad66a08f
seongkyu970 <zxsan1@naver.com>
2020-10-05 19:28:01 +0900

    Update Customer.cpp

--- 20203120_LSK/Customer.cpp
+++ 20203120_LSK/Customer.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 void Customer::Basic(int height,int weight)//키, 몸무게 받아오기
 {
-	Myheight = height;
-	Myweight = weight;
+	MyHeight = height;
+	MyWeight = weight;
 	Pay = 0;//내야할돈 초기화 
 }
 void Customer::Money(int money)// 돈쓰면 private 변수에 저장하기위한 함수  
@@ -20,13 +20,13 @@ void Customer::Ticket(bool free,int num )//자유이용권이면 free 에 트루값, 아니면
 }
 void Customer::CustomerState()//놀이기구 횟수, 내야할돈, 튜브 빌린거 확인 
 {
-	cout<<"내야할 돈 : "<<pay<<endl;
-	cout<<"튜브 여부 : "<<Mytube<<endl;
-	cout<<"놀이기구 이용 횟수"<<endl;
+	cout<<"내야할 돈 : "<<Pay<<endl;
+	cout<<"튜브 여부 : "<<MyTube<<endl;
+	cout<<"놀이기구 이용 횟수"<<Num<<endl;
 }
 void Customer::Exit()
 {
-		cout<<"결제 금액 : "<<pay<<endl;
+		cout<<"결제 금액 : "<<Pay<<endl;
 		cout<<"퇴장 하셨습니다."<<endl;
  
 }
@@ -34,7 +34,7 @@ bool Customer::OutTube()
 {
 	return Mytube;
 }
-int Customer::Outnum()
+int Customer::OutNum()
 {
 	return Num;
 }
